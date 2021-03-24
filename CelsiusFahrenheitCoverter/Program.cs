4e9dfb9ab2b3fa50d2c1d190ef851c67c71af88f
Masrur Sadiev <masrur.sadiev@outlook.com>
2021-03-24 18:52:28 -0400

    CFCoverter

--- CelsiusFahrenheitCoverter/Program.cs
+++ CelsiusFahrenheitCoverter/Program.cs
@@ -9,24 +9,30 @@ namespace CelsiusFahrenheitCoverter
             double f;
             double c;
             string menu;
-            Console.Write("1. Celsius to fahrenhiet");
-            Console.Write("2. Fahrenhiet to celsius");
-            Console.Write("Please chose a number from the menu");
+            Console.WriteLine("1. Celsius to fahrenhiet");
+            Console.WriteLine("2. Fahrenhiet to celsius");
+            Console.Write("Please chose a number from the menu:");
             menu = Console.ReadLine();
             switch (menu)
             {
                 case "1":
-                    Console.Write("Please enter a celsius");
+                    Console.Write("Please enter a celsius:");
                     c = Convert.ToDouble(Console.ReadLine());
                     f = 9 / 5 * c + 32;
-                    Console.Write($"{c} celsius is {f} fahrenheit" );
-                        break;
+                    Console.WriteLine($"{c} celsius is {f} fahrenheit" );
+                    Console.Write("please enter any key to exit");
+                    Console.ReadKey();
+                    break;
                 case "2":
                     Console.Write("Please enter a fahrenheit");
                     f = Convert.ToDouble(Console.ReadLine());
                     c = 9 / 5 * (f - 32);
+                    Console.WriteLine(c);
+                    Console.Write("please enter any key to exit");
+                    Console.ReadKey();
                     break;
                 default:
+                    Console.Write("please enter a number from the menu");
                     break;
             }
 
